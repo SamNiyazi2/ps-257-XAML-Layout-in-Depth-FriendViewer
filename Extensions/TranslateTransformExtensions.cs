de9c622c58e6159f4f0a5f7965e62331c806df1d
Sam Niyazi <sam2003@niyazi.com>
2020-09-29 12:18:37 -0500

    Completed M04-10.

---
+++ Extensions/TranslateTransformExtensions.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Media3D;
+
+
+// 09/29/2020 09:05 am - SSN - [20200929-0720] - [004] - M04-09 - Translate an element
+// 09/29/2020 12:00 pm - SSN - [20200929-1048] - [003] - M04-10 - FriendView: Navigation flyout
+// Copied from "C:\Sams_P\PS\xaml-layout-in-depth\Homework\TranslateTransformWPF\Extensions"
+
+//namespace TranslateTransformWPF.Extensions
+namespace FriendViewer.Extensions
+
+{
+    public static class TranslateTransformExtensions
+    {
+        public static void AnimateTo(this TranslateTransform translateTransform, Point point)
+        {
+
+            StartAnimation(translateTransform, TranslateTransform.XProperty, point.X);
+            StartAnimation(translateTransform, TranslateTransform.YProperty, point.Y);
+        }
+
+        private static void StartAnimation(TranslateTransform translateTransform, DependencyProperty dependencyProperty, double toValue)
+        {
+
+            EasingFunctionBase easingFunctionBase = new QuarticEase();
+            easingFunctionBase = new QuinticEase();
+            easingFunctionBase = new QuarticEase();
+
+
+            var animation = new DoubleAnimation
+            {
+                To = toValue,
+                Duration = new Duration(TimeSpan.FromSeconds(0.7)),
+                EasingFunction = easingFunctionBase
+            };
+
+            translateTransform.BeginAnimation(dependencyProperty, animation);
+
+        }
+
+
+    }
+}
--- MainWindow_v02.xaml.cs
+++ MainWindow_v02.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FriendViewer.Extensions;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Windows;
@@ -20,6 +21,49 @@ namespace FriendViewer
         public WindowWindow_v02()
         {
             InitializeComponent();
+            navigationGrid.Visibility = Visibility.Hidden;
+
+            Loaded += WindowWindow_v02_Loaded;
+        }
+
+        private void WindowWindow_v02_Loaded(object sender, RoutedEventArgs e)
+        {
+            hideNavigator();
+        }
+
+
+        // 09/29/2020 11:56 am - SSN - [20200929-1048] - [002] - M04-10 - FriendView: Navigation flyout
+
+        private void FriendControl_MouseEnter(object sender, MouseEventArgs e)
+        {
+            hideNavigator();
+        }
+
+        private void hideNavigator()
+        {
+
+            GeneralTransform generalTransform = this.TransformToVisual(navigationGrid);
+            Point point = generalTransform.Transform(new Point());
+
+            point.X += navigationTransform.X;
+            point.X -= navigationColumn.ActualWidth;
+            point.Y = 0;
+
+            navigationTransform.AnimateTo(point);
+        }
+
+        // 09/29/2020 12:06 pm - SSN - [20200929-1048] - [004] - M04-10 - FriendView: Navigation flyout
+        private void NavigationButton_MouseEnter(object sender, MouseEventArgs e)
+        {
+
+
+            if (navigationGrid.Visibility != Visibility.Visible)
+            {
+                navigationGrid.Visibility = Visibility.Visible;
+            }
+
+            navigationTransform.AnimateTo(new Point());
+
         }
     }
 }
