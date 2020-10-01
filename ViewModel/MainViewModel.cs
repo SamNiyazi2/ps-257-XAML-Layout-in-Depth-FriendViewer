21e1946f7cd97dc8b8f86109b2b9124a3503d7a6
Sam Niyazi <sam2003@niyazi.com>
2020-10-01 12:20:14 -0500

    Completed M05-12.

--- ViewModel/MainViewModel.cs
+++ ViewModel/MainViewModel.cs
@@ -1,10 +1,12 @@
-﻿using FriendViewer.DataProvider;
+﻿using FriendViewer.Commands;
+using FriendViewer.DataProvider;
 using FriendViewer.DesignTimeData;
 using FriendViewer.Model;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace FriendViewer.ViewModel
 {
@@ -13,6 +15,10 @@ namespace FriendViewer.ViewModel
         private readonly IFriendDataProvider _dataProvider;
         private Friend _selectedFriend;
         public ObservableCollection<Friend> Friends { get; set; }
+
+        // 10/01/2020 11:20 am - SSN - [20201001-1114] - [002] - M05-12 - FriendViewer: New main area
+        public ObservableCollection<Friend> MainAreaFriends { get; set; }
+
 
         private bool _IsLoading;
         public bool IsLoading
@@ -32,14 +38,49 @@ namespace FriendViewer.ViewModel
             {
                 _selectedFriend = value;
                 OnPropertyChanged();
+                // 10/01/2020 11:22 am - SSN - [20201001-1114] - [003] - M05-12 - FriendViewer: New main area
+                if (_selectedFriend != null)
+                {
+                    if (MainAreaFriends.Contains(_selectedFriend))
+                    {
+                        MainAreaFriends.Move(MainAreaFriends.IndexOf(_selectedFriend), 0);
+                    }
+                    else
+                    {
+                        MainAreaFriends.Insert(0, _selectedFriend);
+                    }
+                }
             }
         }
+
+
+        // 10/01/2020 11:24 am - SSN - [20201001-1114] - [004] - M05-12 - FriendViewer: New main area
+        public ICommand CloseMainAreaFriendCommand { get; private set; }
+
+        private void OnCloseMainAreaFriendExecuted(object obj)
+        {
+            var friend = obj as Friend;
+            if (friend != null && MainAreaFriends.Contains(friend))
+            {
+                MainAreaFriends.Remove(friend);
+                if (SelectedFriend == friend)
+                {
+                    SelectedFriend = null;
+                }
+            }
+        }
+
 
 
         public MainViewModel(IFriendDataProvider dataProvider)
         {
             _dataProvider = dataProvider;
             Friends = new ObservableCollection<Friend>();
+            MainAreaFriends = new ObservableCollection<Friend>();
+
+            // 10/01/2020 12:00 pm - SSN - [20201001-1114] - [005] - M05-12 - FriendViewer: New main area
+            CloseMainAreaFriendCommand = new DelegateCommand(OnCloseMainAreaFriendExecuted);
+
             IsLoading = false;
             LoadDataAsync();
         }
